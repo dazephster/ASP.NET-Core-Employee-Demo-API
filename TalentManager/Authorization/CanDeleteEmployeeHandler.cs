1661aeff2655f95bdda922fab318a7c0fe02c535
dazephster <austinlee12378@gmail.com>
2025-09-09 15:07:33 -0500

    CBAC stuff and other small improvements

---
+++ TalentManager/Authorization/CanDeleteEmployeeHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace TalentManager.Authorization
+{
+    using Microsoft.AspNetCore.Authorization;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using TalentManager.Models;
+
+    public sealed class CanDeleteEmployeeHandler : AuthorizationHandler<CanDeleteEmployeeRequirement, Employee>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context, 
+            CanDeleteEmployeeRequirement requirement, 
+            Employee employee)
+        {
+            var user = context.User;
+
+            // Must be in HR Manager role
+            if (!user.IsInRole("Human Resources Manager"))
+                return Task.CompletedTask;
+
+            var dept = user.FindFirst("Department")?.Value;
+            var country = user.FindFirst("Country")?.Value;
+
+            if (dept == employee.Department && country == employee.Country)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- TalentManager/Models/Employee.cs
+++ TalentManager/Models/Employee.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
+        public string Country {  get; set; } = string.Empty;
     }
 }
